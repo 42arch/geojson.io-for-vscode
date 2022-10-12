ecaab302a253c70cc988a19202293cd1f957be23
RE0x2A <rend42@163.com>
2022-10-12 23:56:48 +0800

    chore: update

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'leaflet/dist/leaflet.css'
 import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css'
-// import 'leaflet-draw/dist/leaflet.draw.css'
 import "./index.scss"
 import App from "./components/App"
 
